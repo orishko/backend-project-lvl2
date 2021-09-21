ba2bc3ffdff243417776e82b916d77e66dec0730
Eugene <orishko@mail.com>
2021-09-21 15:35:24 +0300

    add arguments and action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { genDiff } from '../index.js';
 
 const program = new Command();
 
 program
   .usage('[options] <filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  
-program
   .version('0.0.1')
   .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>', 'path to the first file')
+  .argument('<filepath2>', 'path to the second file')
+  .action((filepath1, filepath2) => {
+     console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
