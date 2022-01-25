071a7292f52c605ff0d842dddc63da22ff5779bd
Eugene <orishko@mail.com>
2022-01-25 14:55:49 +0300

    step 4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,7 @@
-/* eslint-disable no-undef */
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import genDiff from '../index.js';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
