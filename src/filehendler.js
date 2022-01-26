897cc57c3d630a1f7dcbcce48267a289be9d9757
Eugene <orishko@mail.com>
2022-01-26 15:12:50 +0300

    fix path to fixtures

--- src/filehendler.js
+++ src/filehendler.js
@@ -1,7 +1,11 @@
 import { readFileSync } from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFullPath = (filepath) => path.resolve(__dirname, '..', '__fixtures__', filepath);
 const getFormat = (filepath) => path.extname(filepath).slice(1);
 
 const getData = (filepath) => readFileSync(filepath, 'utf8');
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const genDiff = (filePath1, filePath2) => {
   const obj1 = readFile(filePath1);
   const obj2 = readFile(filePath2);
   const result = getDifference(obj1, obj2);
-  
   const makeResultString = result.reduce((acc, data) => {
     if (data.status === 'unchanged') acc += `  ${data.key}: ${data.value}\n`;
     if (data.status === 'removed') acc += `- ${data.key}: ${data.value}\n`;
