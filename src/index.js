9da5bc8d77e69b162af6fdd73b72145a08e0f157
Eugene <orishko@mail.com>
2022-01-28 13:39:55 +0300

    add space

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ const genDiff = (filePath1, filePath2) => {
   const obj2 = readFile(filePath2);
   const result = getDifference(obj1, obj2);
   const makeResultString = result.reduce((acc, data) => {
-    if (data.status === 'unchanged') acc += `  ${data.key}: ${data.value}\n`;
-    if (data.status === 'removed') acc += `- ${data.key}: ${data.value}\n`;
-    if (data.status === 'added') acc += `+ ${data.key}: ${data.value}\n`;
-    if (data.status === 'changed') acc += `- ${data.key}: ${data.value1} \n+ ${data.key}: ${data.value2}\n`;
+    if (data.status === 'unchanged') acc += `    ${data.key}: ${data.value}\n`;
+    if (data.status === 'removed') acc += `  - ${data.key}: ${data.value}\n`;
+    if (data.status === 'added') acc += `  + ${data.key}: ${data.value}\n`;
+    if (data.status === 'changed') acc += `  - ${data.key}: ${data.value1}\n  + ${data.key}: ${data.value2}\n`;
     return acc;
   }, '');
 
