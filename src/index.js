2ef640750022feb4de3376c6a159ea135feb1d11
Eugene <orishko@mail.com>
2022-03-29 18:00:55 +0300

    update  makeResultString

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 import getDifference from './getdifference.js';
 import readFile from './parsers.js';
 
+const getKey = (obj) => obj.key;
+const getValue = (obj) => obj.value;
+
 const genDiff = (filePath1, filePath2) => {
   const obj1 = readFile(filePath1);
   const obj2 = readFile(filePath2);
   const result = getDifference(obj1, obj2);
-  const makeResultString = result.reduce((acc, data) => {
-    if (data.status === 'unchanged') acc += `    ${data.key}: ${data.value}\n`;
-    if (data.status === 'removed') acc += `  - ${data.key}: ${data.value}\n`;
-    if (data.status === 'added') acc += `  + ${data.key}: ${data.value}\n`;
-    if (data.status === 'changed') acc += `  - ${data.key}: ${data.value1}\n  + ${data.key}: ${data.value2}\n`;
-    return acc;
-  }, '');
+  console.log(result);
+  const makeResultString = result.map((data) => {
+    const key = getKey(data);
+    const value = getValue(data);
+    switch (data.status) {
+      case 'unchanged':
+        return `    ${key}: ${value}`;
+      case 'removed':
+        return `  - ${key}: ${value}`;
+      case 'added':
+        return `  + ${key}: ${value}`;
+      case 'changed':
+        return `  - ${key}: ${value[0]}\n  + ${key}: ${value[1]}`;
+      default:
+        throw new Error('Unexpected status');
+    }
+  });
 
-  const addBrackets = (str) => `{\n${str}}`;
-
-  return addBrackets(makeResultString);
+  return ['{', ...makeResultString, '}'].join('\n');
 };
 
 export default genDiff;
