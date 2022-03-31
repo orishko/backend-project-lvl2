68f6f850c30f63530419b7f26a82302e1c47cc59
Eugene <orishko@mail.com>
2022-03-31 18:43:47 +0300

    refactor using test.each method

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,34 +9,17 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
-let actualFlat;
-let actualNested;
-let pathJson1;
-let pathJson2;
-let pathYaml1;
-let pathYaml2;
-let pathNested1;
-let pathNested2;
+const cases = [
+  ['file1.json', 'file2.json', 'stylish', 'stylish.txt'],
+  ['file1.yaml', 'file2.yaml', 'stylish', 'stylish.txt'],
+  ['file1.json', 'file2.json', 'plain', 'plain.txt'],
+  ['file1.yaml', 'file2.yaml', 'plain', 'plain.txt'],
+];
 
-beforeAll(() => {
-  actualFlat = readFile('actual_flat.txt', 'utf8');
-  actualNested = readFile('actual_nested.txt', 'utf8');
-  pathJson1 = getFixturePath('file1.json');
-  pathJson2 = getFixturePath('file2.json');
-  pathYaml1 = getFixturePath('file1.yml');
-  pathYaml2 = getFixturePath('file2.yml');
-  pathNested1 = getFixturePath('nested1.json');
-  pathNested2 = getFixturePath('nested2.json');
-});
+test.each(cases)('%# - Compare  %s and %s using %s formatter', (file1, file2, formatter, expected) => {
+  const path1 = getFixturePath(file1);
+  const path2 = getFixturePath(file2);
+  const expectedResult = readFile(expected);
 
-test('comparison two flat json file', () => {
-  expect(genDiff(pathJson1, pathJson2)).toEqual(actualFlat);
-});
-
-test('comparison two flat yaml file', () => {
-  expect(genDiff(pathYaml1, pathYaml2)).toEqual(actualFlat);
-});
-
-test('comparison two nested json file', () => {
-  expect(genDiff(pathNested1, pathNested2)).toEqual(actualNested);
+  expect(genDiff(path1, path2, formatter)).toBe(expectedResult);
 });
